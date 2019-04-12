035a5a3f515847dc3cbe7ccb8ed9d66af01b5301
fabricio.sobral <fabricio.sobral@fireking.com>
2019-04-12 15:59:50 -0300

    version 1

--- public/assets/js/actions.js
+++ public/assets/js/actions.js
@@ -3,43 +3,53 @@ $(document).ready(function ($) {
     $(window).resize(function () {
         responsiveViewClass();
     });
+    getPage(1);
 });
-
-$.getJSON("http://localhost:8888/api/V1/categories/list", function (data) {
-    $(function () {
-        function parseMenu(li, menu) {
-            for (var i = 0; i < menu.length; i++) {
-                var li = $(li).append('<li class="nav-item"><a href="/api/V1/categories/' + menu[i].id + '">' + menu[i].name + '</a></li>');
+function getPage(itemId) {
+    $.getJSON("http://localhost:8888/api/V1/categories/" + itemId, function (data) {
+        $(function () {
+            // console.log(data);
+            function parsePage(element, item) {
+                $(element).empty();
+                for (var i = 0; i < item.length; i++) {
+                    var element = $(element).append('<div id="product" class="col-md-4 col-sm-12">' +
+                        '<p class="thumb"><img src="' + item[i].image + '" class="img-fluid" alt="' + item[i].name + '"></p>' +
+                        '<p class="description">' + item[i].name + '</p>' +
+                        '<p class="price">R$ ' + item[i].price + '</p>' +
+                        '<p><button type="button" class="btn btn-primary btn-lg btn-block">Comprar</button></p>');
+                }
             }
-        }
-        function parseTitle(h1, titlePage) {
-            var h1 = $(h1).append(titlePage[0].name);
+            var elementoDom = $('#products');
+            parsePage(elementoDom, data.items);
+        })
+    })
 
-        }
-        var items = $('#menu ul');
-        parseMenu(items, data.items);
-
-        var items = $('#titlePage h1');
-        parseTitle(items, data.items);
-    });
-
-});
-
-$.getJSON("http://localhost:8888/api/V1/categories/1", function (data) {
-    $(function () {
-        function parsePage(element, item) {
-            for (var i = 0; i < item.length; i++) {
-                var element = $(element).append('<div id="product" class="col-md-4 col-sm-12">' +
-                    '<p class="thumb"><img src="' + item[i].image + '" class="img-fluid" alt="' + item[i].name + '"></p>' +
-                    '<p class="description">' + item[i].name + '</p>' +
-                    '<p class="price">R$ ' + item[i].price + '</p>' +
-                    '<p><button type="button" class="btn btn-primary btn-lg btn-block">Comprar</button></p>');
+    $.getJSON("http://localhost:8888/api/V1/categories/list", function (data2) {
+        $(function () {
+            function parseMenu(li, menu) {
+                $(li).empty();
+                for (var i = 0; i < menu.length; i++) {
+                    var li = $(li).append('<li class="nav-item"><a href="#" onclick="getPage(' + menu[i].id + ')">' + menu[i].name + '</a></li>');
+                }
             }
-        }
-        var items = $('#products');
-        parsePage(items, data.items);
+            var domElementMenu = $('#menu ul');
+            parseMenu(domElementMenu, data2.items);
+
+            function parseTitle(h1, titlePage) {
+                if (itemId == 0) {
+                    $(h1).empty();
+                    var h1 = $(h1).append(titlePage[1].name);
+                } else {
+                    $(h1).empty();
+                    var h1 = $(h1).append(titlePage[itemId].name);
+                }
+
+            }
+            var domElementTitle = $('#titlePage h1');
+            parseTitle(domElementTitle, data2.items);
+        });
     });
-});
+}
 
 $(".navbar-toggler").click(function () {
     $(".main-menu").toggleClass("main-menu-responsive");
